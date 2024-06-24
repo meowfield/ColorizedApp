abbfe80fceaa48278c805c63241f25060bcb4a3f
meowfield <meowfield@yandex.ru>
2024-06-24 19:23:53 +0200

    Initial Commit

---
+++ ColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by Данис Гаязов on 24.6.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
