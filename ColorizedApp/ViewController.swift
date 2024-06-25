bf48935cc8d228fdaee4c3e6b2e545a27e31d797
meowfield <meowfield@yandex.ru>
2024-06-25 03:50:48 +0200

    Initial commit: Create ColorizedApp with basic UI and color mixing functionality

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet var redColorInfo: UILabel!
+    @IBOutlet var greenColorInfo: UILabel!
+    @IBOutlet var blueColorInfo: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var paletteView: UIView!
+  
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        paletteView.layer.cornerRadius = 10
+        mixingColors()
     }
-
-
+    
+    // MARK: - IBActions
+    @IBAction func redSliderAction() {
+        redColorInfo.text = String(format: "%.2f", redSlider.value)
+        mixingColors()
+    }
+        
+    @IBAction func greenSliderAction() {
+        greenColorInfo.text = String(format: "%.2f", greenSlider.value)
+        mixingColors()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueColorInfo.text = String(format: "%.2f", blueSlider.value)
+        mixingColors()
+    }
+    
+    // MARK: - Private Methods
+    private func mixingColors() {
+        paletteView.backgroundColor = UIColor(
+            _colorLiteralRed: redSlider.value,
+            green: greenSlider.value,
+            blue: blueSlider.value,
+            alpha: 1.0
+        )
+    }
+    
 }
 
