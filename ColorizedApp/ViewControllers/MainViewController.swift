892650642f8204af97896172ab3da6055bc2c21f
meowfield <meowfield@yandex.ru>
2024-07-08 23:33:17 +0200

    optimized logic

--- ColorizedApp/ViewControllers/MainViewController.swift
+++ ColorizedApp/ViewControllers/MainViewController.swift
@@ -6,23 +6,17 @@
 //
 
 import UIKit
+// MARK: - Protocols
+protocol ColorDelegate {
+    func sendColor(color: UIColor)
+}
 
 final class MainViewController: UIViewController {
     
     // MARK: - View Lifecycle
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let settingsVC = segue.destination as? SettingsViewController {
-            if let backgroundColor = view.backgroundColor {
-                var red: CGFloat = 0
-                var green: CGFloat = 0.4
-                var blue: CGFloat = 0.4
-                var alpha: CGFloat = 0.4
-                
-                backgroundColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-                settingsVC.red = red
-                settingsVC.green = green
-                settingsVC.blue = blue
-            }
+            settingsVC.color = view.backgroundColor
             settingsVC.delegate = self
         }
     }
@@ -30,7 +24,7 @@ final class MainViewController: UIViewController {
 
 // MARK: - Extensions: ColorDelegate
 extension MainViewController: ColorDelegate {
-    func sendColor(red: Float, green: Float, blue: Float) {
-        view.backgroundColor = UIColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1.0)
+    func sendColor(color: UIColor) {
+        view.backgroundColor = color
     }
 }
--- ColorizedApp/ViewControllers/SettingsViewController.swift
+++ ColorizedApp/ViewControllers/SettingsViewController.swift
@@ -6,20 +6,15 @@
 //
 
 import UIKit
-// MARK: - Protocols
-protocol ColorDelegate {
-    func sendColor(red: Float, green: Float, blue: Float)
-}
+
 
 // MARK: - Class Definition
 final class SettingsViewController: UIViewController {
     
     // MARK: - Properties
     var delegate: ColorDelegate?
+    var color: UIColor!
     
-    var red: CGFloat = 0.0
-    var green: CGFloat = 0.0
-    var blue: CGFloat = 0.0
     
     // MARK: - IBOutlets
     @IBOutlet var redColorInfo: UILabel!
@@ -31,65 +26,29 @@ final class SettingsViewController: UIViewController {
     @IBOutlet var blueSlider: UISlider!
     
     @IBOutlet var paletteView: UIView!
-    @IBOutlet weak var textField: UITextField!
       
     @IBOutlet var redTextField: UITextField!
     @IBOutlet var greenTextField: UITextField!
     @IBOutlet var blueTextField: UITextField!
     
     // MARK: - IBActions
-    @IBAction func updateColorSettingsAfterSlider(_ slider: UISlider) {
+    @IBAction func sliderAction(_ slider: UISlider) {
         switch slider {
         case redSlider:
-            redColorInfo.text = String(format: "%.2f", slider.value)
-            redTextField.text = String(format: "%.2f", slider.value)
-            red = CGFloat(slider.value)
+            setValue(for: redTextField)
+            setValue(for: redColorInfo)
         case greenSlider:
-            greenColorInfo.text = String(format: "%.2f", slider.value)
-            greenTextField.text = String(format: "%.2f", slider.value)
-            green = CGFloat(slider.value)
+            setValue(for: greenTextField)
+            setValue(for: greenColorInfo)
         default:
-            blueColorInfo.text = String(format: "%.2f", slider.value)
-            blueTextField.text = String(format: "%.2f", slider.value)
-            blue = CGFloat(slider.value)
+            setValue(for: blueTextField)
+            setValue(for: blueColorInfo)
         }
         mixingColors()
-    }
+    }    
     
-    @IBAction func updateColorSettingsAfterTextField(_ textfield: UITextField) {
-        switch textfield {
-        case redTextField:
-            if let text = redTextField.text, let value = Float(text) {
-                redSlider.setValue(value, animated: true)
-                redColorInfo.text = String(format: "%.2f", value)
-                red = CGFloat(value)
-            } else {
-                showAlert()
-                { self.redTextField.text = "0.5" }
-            }
-        case greenTextField:
-            if let text = greenTextField.text, let value = Float(text) {
-                greenSlider.setValue(value, animated: true)
-                greenColorInfo.text = String(format: "%.2f", value)
-                green = CGFloat(value)
-            } else {
-                showAlert()
-                { self.greenTextField.text = "0.5" }
-            }
-        default:
-            if let text = blueTextField.text, let value = Float(text) {
-                blueSlider.setValue(value, animated: true)
-                blueColorInfo.text = String(format: "%.2f", value)
-                blue = CGFloat(value)
-            } else {
-                showAlert()
-                { self.blueTextField.text = "0.5" }
-            }
-        }
-        mixingColors()
-    }
     @IBAction func doneButtonTapped() {
-        delegate?.sendColor(red: Float(red), green: Float(green), blue: Float(blue))
+        delegate?.sendColor(color: paletteView.backgroundColor ?? .black)
         navigationController?.popViewController(animated: true)
     }
     
@@ -97,12 +56,10 @@ final class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         paletteView.layer.cornerRadius = 10
-        mixingColors()
         getBasicColorState()
+        mixingColors()
+        
         navigationItem.hidesBackButton = true
-        redTextField.delegate = self
-        greenTextField.delegate = self
-        blueTextField.delegate = self
     }
     
     // MARK: - Public Methods
@@ -110,38 +67,58 @@ final class SettingsViewController: UIViewController {
         super.touchesBegan(touches, with: event)
         view.endEditing(true)
     }
-        
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-        
+                
     // MARK: - Private Methods
+        
+    private func string(from: UISlider) -> String {
+        String(format: "%.2f", from.value)
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redColorInfo: label.text = string(from: redSlider)
+            case greenColorInfo: label.text = string(from: greenSlider)
+            default: label.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    private func setValue(for textFields: UITextField...) {
+        textFields.forEach { textField in
+            switch textField {
+            case redTextField: textField.text = string(from: redSlider)
+            case greenTextField: textField.text = string(from: greenSlider)
+            default: textField.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    private func setValue(for sliders: UISlider...) {
+        let color = CIColor(color: color)
+        sliders.forEach { slider in
+            switch slider {
+            case redSlider: slider.value = Float(color.red)
+            case greenSlider: slider.value = Float(color.green)
+            default: slider.value = Float(color.blue)
+            }
+        }
+    }
+    
     private func getBasicColorState() {
-        redSlider.value = Float(red)
-        greenSlider.value = Float(green)
-        blueSlider.value = Float(blue)
-        redColorInfo.text = String(format: "%.2f", red)
-        greenColorInfo.text = String(format: "%.2f", green)
-        blueColorInfo.text = String(format: "%.2f", blue)
-        redTextField.text = String(format: "%.2f", red)
-        greenTextField.text = String(format: "%.2f", green)
-        blueTextField.text = String(format: "%.2f", blue)
+        setValue(for: redSlider, greenSlider, blueSlider)
+        setValue(for: redColorInfo, greenColorInfo, blueColorInfo)
+        setValue(for: redTextField, greenTextField, blueTextField)
     }
     
     private func mixingColors() {
-            paletteView.backgroundColor = UIColor(
-                _colorLiteralRed: Float(red),
-                green: Float(green),
-                blue: Float(blue),
-                alpha: 1.0
-            )
-        }
+        paletteView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    }
     
     private func showAlert(completion: (() -> Void)? = nil) {
         let alert = UIAlertController(
             title: "Hey!",
-            message: "Please, use only numbers!",
+            message: "Please, use numbers from 0 to 1!",
             preferredStyle: .alert
         )
         alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { _ in
@@ -153,4 +130,59 @@ final class SettingsViewController: UIViewController {
 
 // MARK: - Extensions: UITextFieldDelegate
 extension SettingsViewController: UITextFieldDelegate {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text else {
+            showAlert() {
+                textField.text = "0.50"
+            }
+            return
+        }
+        
+        guard let value = Float(text), (0...1).contains(value) else {
+            showAlert() {
+                textField.text = "0.50"
+            }
+            return
+        }
+        
+        switch textField {
+        case redTextField:
+            redSlider.setValue(value, animated: true)
+            setValue(for: redColorInfo)
+        case greenTextField:
+            greenSlider.setValue(value, animated: true)
+            setValue(for: greenColorInfo)
+        default:
+            blueSlider.setValue(value, animated: true)
+            setValue(for: blueColorInfo)
+        }
+        mixingColors()
     }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let keyboardToolbar = UIToolbar()
+        keyboardToolbar.sizeToFit()
+        textField.inputAccessoryView = keyboardToolbar
+        
+        let doneButton = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: textField,
+            action: #selector(resignFirstResponder)
+        )
+        
+        let flexBarButton = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: nil,
+            action: nil
+        )
+        
+        keyboardToolbar.items = [flexBarButton, doneButton]
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
